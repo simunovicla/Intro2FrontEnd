80c749e86ec8e47ce3f1b525ccc9822f8e284483
simunovicla <90116835+simunovicla@users.noreply.github.com>
2023-03-16 18:01:58 -0300

    Trabajo Primera clase

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight).position(0,0).style('z-index',-1);
+  createA("https://github.com/simunovicla","Mi Súper cuenta de Github","_blank").id("cuenta");
+  textSize(90)
+}
+
+function draw() {
+text("☣",mouseX,mouseY);
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+}
